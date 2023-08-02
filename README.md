0cbfdeb4753f56062603b67740d5a97106728ddb
Gary <wklu99@gmail.com>
2023-08-02 14:42:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test
+
+I am testing this for AlgoKit usage with my course. Please do not use this template yet.
---
+++ templates/{% if template == 'hello_world' %}hello_world{% endif %}/tests/test_hello_world.py
@@ -0,0 +1,6 @@
+class TestHelloWorld:
+    def setup_method(self):
+        ...
+
+    def test_greeting(self):
+        print("Hello, world!")
